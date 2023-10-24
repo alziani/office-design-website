21fb2b2100e8890ae149c1ba535dadd412d690e8
ziani abderrahim <zianiabderrahim@gmail.com>
2023-10-24 22:48:48 +0100

    details & services sections

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const navbar = document.querySelector(".navbar");
 const arrow = document.querySelector(".nar");
 
+const customer = document.getElementById("customer");
+const solved = document.getElementById("solved");
+const reviews = document.getElementById("reviews");
+const studies = document.getElementById("studies");
+
 
 function userScroll() {
     window.addEventListener("scroll", () => {
@@ -22,3 +27,25 @@ function userScroll() {
   });
   document.addEventListener("DOMContentLoaded", userScroll);
   
+
+  let coun = document.querySelectorAll(".counters");
+
+window.addEventListener("load", () => {
+
+
+    coun.forEach(e => {
+        e.innerText = 0;
+
+        let gaol = e.dataset.target;
+        let increment = gaol / 100
+
+        let count = setInterval(() => {
+
+            e.textContent = Math.ceil(+e.textContent + increment);
+           
+            if (e.textContent == gaol) {
+                clearInterval(count)
+            }
+        }, 20)
+    })
+});
